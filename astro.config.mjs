67c220a616323caa80107357de71b641e0a51e94
fairplaay <fairplaay16@gmail.com>
2026-04-05 14:53:49 +0000

    fix: use static import for sanity

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,7 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import sitemap from '@astrojs/sitemap';
-
-const sanity = (await import('@sanity/astro')).default;
+import sanity from '@sanity/astro';
 
 export default defineConfig({
   site: 'https://jesusgarcia.dev',
